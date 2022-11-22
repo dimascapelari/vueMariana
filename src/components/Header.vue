5066c070cd47a98c7df1a8047596b8662165849d
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2022-11-22 16:56:22 -0300

    exercicios

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1 v-if="titulo == 'Página Header'">{{ titulo }}</h1>
-  <h1 v-else>Título 2</h1>
-  <input type="text" v-model="textoInput" />
-  <h2>{{ textoInput }}</h2>
-  <img :src="imageSrc" /><br />
-  <button @click="deletaTitulo">Deletar Título</button>
+  <div ref="container">
+    <div ref="titulo">
+      <h1 v-if="titulo == 'Página Header'">{{ titulo }}</h1>
+      <h1 v-else>Título 2</h1>
+      <input type="text" v-model="textoInput" />
+      <h2>{{ textoInput }}</h2>
+    </div>
+    <img :src="imageSrc" /><br />
+
+    <input
+      ref="btn"
+      type="button"
+      value="Deletar Título"
+      @click="deletaTitulo"
+      @change="voltarTitulo"
+    />
+  </div>
 </template>
 
 <script>
@@ -19,8 +30,22 @@ export default {
   },
   methods: {
     deletaTitulo() {
-      this.titulo = "";
+      if (this.titulo == "Página Header") {
+        this.titulo = "";
+        this.$refs.btn.value = "Voltar Título";
+      } else {
+        this.titulo = "Página Header";
+        this.$refs.btn.value = "Deletar Título";
+      }
     },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.$refs.container.style.height = "500px";
+      this.$refs.container.style.backgroundColor = "black";
+      this.$refs.container.style.textAlign = "center";
+      this.$refs.titulo.style.color = "#fff";
+    }, 1000);
   },
 };
 </script>
