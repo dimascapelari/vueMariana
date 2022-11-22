5a558e8097d90d15aea02dce80a7ff492fb59dbb
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2022-11-22 16:23:23 -0300

    exercicio mariana

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 v-if="titulo == 'Página Header'">{{ titulo }}</h1>
+  <h1 v-else>Título 2</h1>
+  <input type="text" v-model="textoInput" />
+  <h2>{{ textoInput }}</h2>
+  <img :src="imageSrc" /><br />
+  <button @click="deletaTitulo">Deletar Título</button>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      titulo: "Página Header",
+      textoInput: "",
+      imageSrc: "/images/download.png",
+    };
+  },
+  methods: {
+    deletaTitulo() {
+      this.titulo = "";
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style></style>
